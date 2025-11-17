84dc96b659a5970899bdbcf9c829d353ffdce015
edumu <eduardoaum2007@gmail.com>
2025-11-17 16:23:54 -0300

    backend folder

---
+++ back-end/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 http://localhost:${port}`);
+});
